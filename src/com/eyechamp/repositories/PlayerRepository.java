a6801217c25f2b268645c07edfffc1d8c2a5e71e
Michel Meeuwissen <mmeeuw@gmail.com>
2015-02-22 12:16:40 +0100

    Removed the spring config xml files

--- src/com/eyechamp/repositories/PlayerRepository.java
+++ src/com/eyechamp/repositories/PlayerRepository.java
@@ -3,8 +3,9 @@ package com.eyechamp.repositories;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.eyechamp.model.Player;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface PlayerRepository extends MongoRepository<Player, String> {	
 	public Player findByName(String name);
 }
